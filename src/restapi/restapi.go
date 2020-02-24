64f47738d0b509adcfca5698d1ca780b08ae4c38
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-02-24 10:05:33 +0100

    Split restapi.go.

--- src/restapi/restapi.go
+++ src/restapi/restapi.go
@@ -8,9 +8,7 @@ import (
 	"io/ioutil"
     "log"
 	"net/http"
-	"strconv"
 	"github.com/catenocrypt/nano-work-cache/workcache"
-	"github.com/catenocrypt/nano-work-cache/rpcclient"
 )
 
 type actionJson struct {
@@ -90,133 +88,6 @@ type accountsBalancesJson struct {
 func workResponseToJson(resp workcache.WorkResponse) string {
 	return fmt.Sprintf(`{"hash":"%v","work":"%v","difficulty":"%x","multiplier":"%v","source":"%v"}`,
 		resp.Hash, resp.Work, resp.Difficulty, resp.Multiplier, resp.Source)
-}
-
-func handleJson(action string, respBody []byte, w http.ResponseWriter) {
-	switch action {
-	case "work_generate":
-		var workGenerate workGenerateJson
-		err := json.Unmarshal(respBody, &workGenerate)
-		if err != nil {
-			fmt.Fprintln(w, `{"error":"work_generate parse error"}`)
-			return
-		}
-		log.Println("work_generate req", workGenerate)
-		var difficulty uint64 = workcache.GetDefaultDifficulty()
-		if (len(workGenerate.Difficulty) > 0) {
-			difficultyParsed, err := strconv.ParseUint(workGenerate.Difficulty, 16, 64)
-			if (err != nil) {
-				// diff present, but could not parse
-				fmt.Fprintln(w, `{"error":"work_generate difficulty parse error"}`)
-				return
-			}
-			difficulty = difficultyParsed
-		}
-		// handle
-		workResp, err := workcache.GetCachedWork(nanoNodeUrl, workGenerate.Hash, difficulty, "")
-		if (err != nil) {
-			fmt.Fprintln(w, `{"error":"` + err.Error() + `"}`)
-			return
-		}
-		log.Println("work_generate resp", workResp)
-		fmt.Fprintln(w, workResponseToJson(workResp))
-		break
-
-	case "work_pregenerate_by_hash":
-		var workPregenerateByHash workPregenerateByHashJson
-		err := json.Unmarshal(respBody, &workPregenerateByHash)
-		if err != nil {
-			fmt.Fprintln(w, `{"error":"work_pregenerate_by_hash parse error"}`)
-			return
-		}
-		log.Println("work_pregenerate_by_hash req", workPregenerateByHash)
-		var hash = workPregenerateByHash.Hash
-		var difficulty uint64 = workcache.GetDefaultDifficulty()
-		// start work asynchronously
-		go workcache.GetCachedWork(nanoNodeUrl, hash, difficulty, "")
-		// return response, only hash
-		fmt.Fprintln(w, fmt.Sprintf(`{"hash":"%v","source":"started_in_background"}`, hash))
-		return
-
-	case "work_pregenerate_by_account":
-		var workPregenerateByAccount workPregenerateByAccountJson
-		err := json.Unmarshal(respBody, &workPregenerateByAccount)
-		if err != nil {
-			fmt.Fprintln(w, `{"error":"work_pregenerate_by_account parse error"}`)
-			return
-		}
-		log.Println("work_pregenerate_by_account req", workPregenerateByAccount)
-		var account = workPregenerateByAccount.Account
-		// get frontier and start work asynchronously
-		go workcache.GetCachedWorkByAccount(nanoNodeUrl, account)
-		// return response, only account is echoed back, hash and work is not available yet
-		fmt.Fprintln(w, fmt.Sprintf(`{"account":"%v","hash":"<retrieve_in_progress>","source":"started_in_background"}`, account))
-		return
-
-	case "account_balance":
-		// account_balance also triggers work_precompute in the background, and transparently proxies the call for balance
-		var accountBalance accountBalanceJson
-		err := json.Unmarshal(respBody, &accountBalance)
-		if err != nil {
-			fmt.Fprintln(w, `{"error":"account_balance parse error"}`)
-			return
-		}
-		log.Println("account_balance", accountBalance)
-
-		// get frontier and start work asynchronously
-		go workcache.GetCachedWorkByAccount(nanoNodeUrl, accountBalance.Account)
-
-		// proxy the call
-		respJSON, err := rpcclient.MakeGenericCall(nanoNodeUrl, string(respBody))
-		if (err != nil) {
-			log.Println("RPC error:", err.Error())
-			fmt.Fprintln(w, `{"error":"RPC error: ` + err.Error() + `","action":"` + action + `"}`)
-			return
-		}
-		fmt.Fprintln(w, respJSON)
-		break
-		
-	case "accounts_balances":
-		// accounts_balances also triggers work_precompute (for all accounts) in the background, and transparently proxies the call for balances
-		var accountsBalances accountsBalancesJson
-		err := json.Unmarshal(respBody, &accountsBalances)
-		if err != nil {
-			fmt.Fprintln(w, `{"error":"accounts_balances parse error"}`)
-			return
-		}
-		log.Println("accounts_balances", accountsBalances)
-
-		// for all accounts get frontier and start work asynchronously
-		for _, account := range accountsBalances.Accounts {
-			go workcache.GetCachedWorkByAccount(nanoNodeUrl, account)
-		}
-
-		// proxy the call
-		respJSON, err := rpcclient.MakeGenericCall(nanoNodeUrl, string(respBody))
-		if (err != nil) {
-			log.Println("RPC error:", err.Error())
-			fmt.Fprintln(w, `{"error":"RPC error: ` + err.Error() + `","action":"` + action + `"}`)
-			return
-		}
-		fmt.Fprintln(w, respJSON)
-		break
-
-	case "nano-work-cache-status":
-		status := getStatus()
-		fmt.Fprintln(w, status)
-		break
-
-	default:
-		// proxy any other request unmodified
-		log.Println("transaprent proxying of action", action)
-		respJSON, err := rpcclient.MakeGenericCall(nanoNodeUrl, string(respBody))
-		if (err != nil) {
-			log.Println("RPC error:", err.Error())
-			fmt.Fprintln(w, `{"error":"RPC error: ` + err.Error() + `","action":"` + action + `"}`)
-			return
-		}
-		fmt.Fprintln(w, respJSON)
-	}
 }
 
 var nanoNodeUrl string
