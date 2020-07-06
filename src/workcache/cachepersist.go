4a490ca0ed55e83a2adc1e3b221a4d98e3067b1f
Adam R <13562139+catenocrypt@users.noreply.github.com>
2020-07-06 17:33:01 +0200

    At startup try to load backup cache file as well if cache file cannot be loaded (#8)
    * Formatting changes.
    * At startup try to load backup cache file as well if cache file cannot be loaded.
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>

--- src/workcache/cachepersist.go
+++ src/workcache/cachepersist.go
@@ -11,7 +11,9 @@ import (
 
 func isPersistToFileEnabled() bool {
 	filename := ConfigGetString("Main.CachePeristFileName")
-	if len(filename) == 0 { return false }
+	if len(filename) == 0 {
+		return false
+	}
 	return true
 }
 
@@ -22,35 +24,49 @@ func persistFileName() string {
 
 // SaveCache Save the cache to file or other persistence configured
 func SaveCache() {
-	if !isPersistToFileEnabled() { return }
+	if !isPersistToFileEnabled() {
+		return
+	}
 	saveToFile(persistFileName())
+}
+
+func backupFileName(filename string) string {
+	return filename + ".bak"
 }
 
 // LoadCache Load the cache from file or other persistence configured
 func LoadCache() {
-	if !isPersistToFileEnabled() { return }
-	loadFromFile(persistFileName())
+	if !isPersistToFileEnabled() {
+		return
+	}
+	filename := persistFileName()
+	err := loadFromFile(filename)
+	if err == nil {
+		return
+	}
+	// try bak file
+	_ = loadFromFile(backupFileName(filename))
 }
 
 // saveToFile save cache to the given file
 func saveToFile(filename string) {
 	// first try to rename old file to .bak (ignore error if not possible / not exists)
-	fileNameBak := filename + ".bak"
+	fileNameBak := backupFileName(filename)
 	_ = os.Remove(fileNameBak)
 	_ = os.Rename(filename, fileNameBak)
 	// open new file for writing
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		log.Println("Error saving cache to file, could not open file", err.Error())
+		log.Println("Error saving cache to file, could not open file;", err.Error())
 		return
 	}
 	defer file.Close()
 
 	var cnt int = 0
 	for _, entry := range workCache {
-		line := entryToString(entry);
+		line := entryToString(entry)
 		if len(line) > 0 {
-			fmt.Fprintln(file, entryToString(entry));
+			fmt.Fprintln(file, entryToString(entry))
 			cnt++
 		}
 	}
@@ -58,31 +74,37 @@ func saveToFile(filename string) {
 }
 
 // loadFromFile Read cache entries from the given file, merge them with current cache
-func loadFromFile(filename string) {
+func loadFromFile(filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Println("Error loading cache from file, could not open file", err.Error())
-		return
+		log.Println("Error loading cache from file, could not open file;", err.Error())
+		return err
 	}
 	defer file.Close()
 
 	var cnt int = 0
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
 		// one entry is one line
 		line := scanner.Text()
 		var entry CacheEntry
 		lineParsed := entryLoadFromString(line, &entry)
-		if !lineParsed { continue; }
+		if !lineParsed {
+			continue
+		}
 		// omit non-"valid" entries
-		if entry.status != "valid" { continue; }
+		if entry.status != "valid" {
+			continue
+		}
 		addToCacheInternal(entry)
 		cnt++
 	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return err
 	}
 
-	log.Printf("Cache loaded from file, %v entries read, %v entries\n", cnt, StatusCacheSize())
+	log.Printf("Cache loaded from file %v, %v entries read, %v entries\n", filename, cnt, StatusCacheSize())
+	return nil
 }
