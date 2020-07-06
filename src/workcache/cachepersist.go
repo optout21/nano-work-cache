1a9f300a7a9ba95cd7167f381576f206c19f8c90
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-07-06 17:07:34 +0200

    Formatting changes.

--- src/workcache/cachepersist.go
+++ src/workcache/cachepersist.go
@@ -11,7 +11,9 @@ import (
 
 func isPersistToFileEnabled() bool {
 	filename := ConfigGetString("Main.CachePeristFileName")
-	if len(filename) == 0 { return false }
+	if len(filename) == 0 {
+		return false
+	}
 	return true
 }
 
@@ -22,13 +24,17 @@ func persistFileName() string {
 
 // SaveCache Save the cache to file or other persistence configured
 func SaveCache() {
-	if !isPersistToFileEnabled() { return }
+	if !isPersistToFileEnabled() {
+		return
+	}
 	saveToFile(persistFileName())
 }
 
 // LoadCache Load the cache from file or other persistence configured
 func LoadCache() {
-	if !isPersistToFileEnabled() { return }
+	if !isPersistToFileEnabled() {
+		return
+	}
 	loadFromFile(persistFileName())
 }
 
@@ -41,16 +47,16 @@ func saveToFile(filename string) {
 	// open new file for writing
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		log.Println("Error saving cache to file, could not open file", err.Error())
+		log.Println("Error saving cache to file, could not open file;", err.Error())
 		return
 	}
 	defer file.Close()
 
 	var cnt int = 0
 	for _, entry := range workCache {
-		line := entryToString(entry);
+		line := entryToString(entry)
 		if len(line) > 0 {
-			fmt.Fprintln(file, entryToString(entry));
+			fmt.Fprintln(file, entryToString(entry))
 			cnt++
 		}
 	}
@@ -61,28 +67,32 @@ func saveToFile(filename string) {
 func loadFromFile(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Println("Error loading cache from file, could not open file", err.Error())
+		log.Println("Error loading cache from file, could not open file;", err.Error())
 		return
 	}
 	defer file.Close()
 
 	var cnt int = 0
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
 		// one entry is one line
 		line := scanner.Text()
 		var entry CacheEntry
 		lineParsed := entryLoadFromString(line, &entry)
-		if !lineParsed { continue; }
+		if !lineParsed {
+			continue
+		}
 		// omit non-"valid" entries
-		if entry.status != "valid" { continue; }
+		if entry.status != "valid" {
+			continue
+		}
 		addToCacheInternal(entry)
 		cnt++
 	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 
-	log.Printf("Cache loaded from file, %v entries read, %v entries\n", cnt, StatusCacheSize())
+	log.Printf("Cache loaded from file %v, %v entries read, %v entries\n", filename, cnt, StatusCacheSize())
 }
